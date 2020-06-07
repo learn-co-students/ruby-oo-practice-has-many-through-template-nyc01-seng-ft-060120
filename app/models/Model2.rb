f5dae9af83a57bd022169ab575d9b20bef36a436
dr-dolce14 <dolcemascop1@gmail.com>
2020-06-07 11:58:12 -0400

    Done.

--- app/models/Model2.rb
+++ app/models/Model2.rb
@@ -1,5 +1,25 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Ghost
+
+    attr_reader :house_name, :death
+    attr_accessor :nickname, :ghost_hunter
+
+    @@all = []
+
+    def initialize(nickname, house_name, ghost_hunter, death)
+        @nickname = nickname
+        @house_name = house_name
+        @ghost_hunter = ghost_hunter
+        @death = death
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+
+ 
 
 end
